e514b971d2ffbb08b473dd4d230a9e5145a51a1b
tony <gwjlshebei@gmail.com>
2021-12-04 22:11:50 +0800

    驼峰命名两种解法

--- src/1.CapitalizeString.ts
+++ src/1.CapitalizeString.ts
@@ -1,3 +1,4 @@
+// 目的就是实现一个内置的Capitalize工具方法
 type CapitalizeString<T> = T extends `${infer L}${infer R}` ? `${Uppercase<L>}${R}` : T;
 
 type a1 = CapitalizeString<"handler">; // Handler
--- src/10.camelCase.ts
+++ src/10.camelCase.ts
@@ -1,7 +1,14 @@
+// 解法一，利用了之前的字符串开头大写类型
 type CapitalizeString<T> = T extends `${infer L}${infer R}` ? `${Uppercase<L>}${R}` : T;
 type CamelCase<T extends string, Result extends string = ""> = T extends `${infer L}-${infer R}`
-  ? `${Result}${CamelCase<R, CapitalizeString<L>>}`
+  ? CamelCase<R, `${Result}`>
   : `${Result}${CapitalizeString<T>}`;
+
+// 解法2. 遍历的时候老方法，
+// type CamelCase<T extends string, Result extends string = ""> = T extends `${infer L}-${infer R1}${infer R2}`
+//   ? CamelCase<R2, `${Result}${L}${Uppercase<R1>}`>
+//   : Capitalize<`${Result}${T}`>;
+
 type a1 = CamelCase<"handle-open-flag">; // HandleOpenFlag
 type a2 = CamelCase<"open-flag">; // OpenFlag
 
