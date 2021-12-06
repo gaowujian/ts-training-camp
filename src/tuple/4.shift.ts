ff137e31e2587f98eba1acd15589572fdce69c10
tony <gwjlshebei@gmail.com>
2021-12-06 11:06:17 +0800

    第四题删除元祖第一个类型

---
+++ src/tuple/4.shift.ts
@@ -0,0 +1,6 @@
+type Shift<T extends any[]> = T extends [infer L, ...infer R] ? R : T;
+type A = Shift<[1, 2, 3]>; // [2,3]
+type B = Shift<[1]>; // []
+type C = Shift<[]>; // []
+
+export default {};
