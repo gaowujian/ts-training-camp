ff7867f1d84648fe332dae55a27481502f6af777
tony <gwjlshebei@gmail.com>
2021-12-06 10:48:35 +0800

    update

--- note.md
+++ note.md
@@ -6,10 +6,33 @@
 
 对于一个字符串类型来说，通过 `${infer L}${infer R}` 进行判断就是在尝试去分割一个字符串，直到最后的一次分割结果就是无法继续分割, L 是最后一个字符，而 R 是一个空。 这是一个遍历字符串类型最基本的范式。
 
-## 遍历字符串，并对遍历的所有字符串进行操作，
+# 遍历字符串，并对遍历的所有字符串进行操作，
 
 type UpperString<T extends string, Result extends string = ""> = T extends `${infer L}${infer R}`
 ? UpperString<R, `${Result}${Uppercase<L>}`>
 : Result;
 
 type c = UpperString<"handle">;
+
+## ObjectAccessPaths
+
+传入一个对象给泛型 T 之后，等下次函数调用的时候只能用属性名的连接
+
+## componentEmitsTypes
+
+获取组件的派发事件类型，在传入一个对象给泛型 T 之后，等下次使用组件的时候，需要传入指定格式的事件绑定函数
+
+在 vue 中的 createComponent 用到，不过在 react 中，在编写一个组件的时候，大部分操作是直接手动编写好组件需要哪些 props
+
+```
+interface CollapseProps {
+  onChange?: () => void;
+  onClick?: () => void;
+}
+
+export const Collapse: React.FC<CollapseProps> = () => {
+  return <div></div>;
+};
+
+const b = <Collapse onChange={() => {}} />;
+```
--- src/10.camelCase.ts
+++ src/10.camelCase.ts
@@ -1,13 +1,13 @@
 // 解法一，利用了之前的字符串开头大写类型
-type CapitalizeString<T> = T extends `${infer L}${infer R}` ? `${Uppercase<L>}${R}` : T;
-type CamelCase<T extends string, Result extends string = ""> = T extends `${infer L}-${infer R}`
-  ? CamelCase<R, `${Result}`>
-  : `${Result}${CapitalizeString<T>}`;
+// type CapitalizeString<T> = T extends `${infer L}${infer R}` ? `${Uppercase<L>}${R}` : T;
+// export type CamelCase<T extends string, Result extends string = ""> = T extends `${infer L}-${infer R}`
+//   ? CamelCase<R, `${Result}`>
+//   : `${Result}${CapitalizeString<T>}`;
 
 // 解法2. 遍历的时候老方法，
-// type CamelCase<T extends string, Result extends string = ""> = T extends `${infer L}-${infer R1}${infer R2}`
-//   ? CamelCase<R2, `${Result}${L}${Uppercase<R1>}`>
-//   : Capitalize<`${Result}${T}`>;
+export type CamelCase<T extends string, Result extends string = ""> = T extends `${infer L}-${infer R1}${infer R2}`
+  ? CamelCase<R2, `${Result}${L}${Uppercase<R1>}`>
+  : Capitalize<`${Result}${T}`>;
 
 type a1 = CamelCase<"handle-open-flag">; // HandleOpenFlag
 type a2 = CamelCase<"open-flag">; // OpenFlag
